b9d935e8d3f623b46eb89b4a42e0c59277d0379b
derkdev976-web <derkdev976@gmail.com>
2026-04-13 15:36:32 +0200

    Added cybersecurity response system

--- CybersecurityChatbot/Chatbot.cs
+++ CybersecurityChatbot/Chatbot.cs
@@ -26,6 +26,32 @@ public class Chatbot
         Console.WriteLine("Welcome!");
         user.GetUserInfo();
         Console.WriteLine($"Hello {user.Name}!");
+
+       
+
+        while (true)
+        {
+            Console.Write("> ");
+            string input = Console.ReadLine()?.ToLower() ?? "";
+
+            Console.WriteLine(GetResponse(input));
+
+            if (input == "exit") break;
+        }
+    }
+
+    private string GetResponse(string input)
+    {
+        if (input.Contains("password"))
+            return "Use strong passwords!";
+        else if (input.Contains("phishing"))
+            return "Don't click suspicious links!";
+        else if (input == "help")
+            return "Ask about cybersecurity topics!";
+        else if (input == "exit")
+            return "Goodbye!";
+        else
+            return "I don't understand.";
     }
 
     
